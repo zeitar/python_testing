ffb0a3554fc59b01d5dc4b6d8d4258d7b4fae205
zeitar <150230975+zeitar@users.noreply.github.com>
2024-01-25 01:34:36 +0300

    Add files via upload

---
+++ TelegramBot/config.py
@@ -0,0 +1,9 @@
+TOKEN = "5997237367:AAFprYzKZmHPPPs_Ymw2v7_sQwQwf_5qvS8"
+keys = {
+    'биткоин': 'BTC',
+    'эфир': 'ETH',
+    'солана': 'SOL',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+}
