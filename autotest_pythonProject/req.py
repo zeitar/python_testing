90bf756fb01ae052d4f471118f7fa8475e6d31df
zeitar <150230975+zeitar@users.noreply.github.com>
2024-02-07 17:14:34 +0300

    Delete autotest_pythonProject directory

--- autotest_pythonProject/req.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-status = 'available'
-
-res = requests.get( f"https://petstore.swagger.io/v2/pet/findByStatus?status={status}", headers = {'accept': 'application/json'})
-print(res.text)
-print(res.json)
-print(type(res.json()))
